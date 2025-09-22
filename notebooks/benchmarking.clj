330164b37c587d184e6076e63efac9e161298b64
Seb Bailey <seb231@me.com>
2025-09-22 14:34:17 +0100

    benchmarking notebook

---
+++ notebooks/benchmarking.clj
@@ -0,0 +1,13 @@
+^{:nextjournal.clerk/toc true}
+(ns benchmarking
+  {:nextjournal.clerk/visibility {:code   :hide
+                                  :result :hide}
+   :nextjournal.clerk/page-size            nil
+   :nextjournal.clerk/auto-expand-results? true
+   :nextjournal.clerk/budget               nil}
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk-slideshow :as slideshow]
+            [tablecloth.api :as tc]
+            [witan.send.adroddiad.clerk.html :as chtml]
+            [witan.homelessness.benchmarking.assessments :as bass]
+            ))
